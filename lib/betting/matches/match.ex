23aeb764d4d2e79d621835f8b83abaf90b066250
Michael Kamau <kamau.karitu@gmail.com>
2025-02-11 16:10:46 +0300

    Validate teams to not be same

--- lib/betting/matches/match.ex
+++ lib/betting/matches/match.ex
@@ -19,5 +19,17 @@ defmodule Betting.Matches.Match do
     match
     |> cast(attrs, [:team_a_score, :team_b_score, :team_a_id, :team_b_id, :odd_id, :category_id])
     |> validate_required([:team_a_score, :team_b_score, :team_a_id, :team_b_id, :category_id])
+    |> validate_team_ids_unique()
+  end
+
+  defp validate_team_ids_unique(changeset) do
+    team_a_id = get_field(changeset, :team_a_id)
+    team_b_id = get_field(changeset, :team_b_id)
+
+    if team_a_id == team_b_id do
+      add_error(changeset, :team_b_id, "Team A and Team B cannot be the same")
+    else
+      changeset
+    end
   end
 end
